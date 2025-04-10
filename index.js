94c6ba62f5611583cea0b384f460c61fcff4e3ba
Atte Jauhiainen <atte.jauhiainen@azsh.fi>
2025-04-10 13:19:03 +0300

    enumerate devices

--- index.js
+++ index.js
@@ -15,6 +15,23 @@
     let canvas = null;
     let photo = null;
     let startButton = null;
+
+    if (!navigator.mediaDevices?.enumerateDevices) {
+      console.log("enumerateDevices() not supported.");
+    } else {
+      // List cameras and microphones.
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          devices.forEach((device) => {
+            console.log(`${device.kind}: ${device.label} id = ${device.deviceId}`);
+          });
+        })
+        .catch((err) => {
+          console.error(`${err.name}: ${err.message}`);
+        });
+    }
+    
   
     function showViewLiveResultButton() {
       if (window.self !== window.top) {
